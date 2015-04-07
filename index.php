2fe329b42605544ff74cfebed75216ef62a63179
Florian <laforest.florian@gmail.com>
2015-04-07 17:25:48 +0200

    correctif

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
-	session_start();
 	error_reporting(E_ALL);
 	ini_set('error_reporting', E_ALL);
+	session_start();
 	require("facebook-php-sdk-v4-4.0-dev/autoload.php");
 
 	use Facebook\FacebookSession;
