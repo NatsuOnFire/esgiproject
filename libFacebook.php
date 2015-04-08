38a89bbae93d5a133b6c84f2edea3c7d79c2d71a
Florian <laforest.florian@gmail.com>
2015-04-08 21:49:01 +0200

    test

--- libFacebook.php
+++ libFacebook.php
@@ -1,14 +1,14 @@
 <?php
 	require("facebook-php-sdk-v4-4.0-dev/autoload.php");
 	
-	require_once( 'facebook-php-sdk-v4-4.0-dev/Facebook/FacebookSession.php' );
-	require_once( 'facebook-php-sdk-v4-4.0-dev/Facebook/FacebookRedirectLoginHelper.php' );
-	require_once( 'facebook-php-sdk-v4-4.0-dev/Facebook/FacebookRequest.php' );
-	require_once( 'facebook-php-sdk-v4-4.0-dev/Facebook/FacebookResponse.php' );
-	require_once( 'facebook-php-sdk-v4-4.0-dev/Facebook/FacebookSDKException.php' );
-	require_once( 'facebook-php-sdk-v4-4.0-dev/Facebook/FacebookRequestException.php' );
-	require_once( 'facebook-php-sdk-v4-4.0-dev/Facebook/FacebookAuthorizationException.php' );
-	require_once( 'facebook-php-sdk-v4-4.0-dev/Facebook/GraphObject.php' );
+	require_once( 'facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookSession.php' );
+	require_once( 'facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookRedirectLoginHelper.php' );
+	require_once( 'facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookRequest.php' );
+	require_once( 'facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookResponse.php' );
+	require_once( 'facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookSDKException.php' );
+	require_once( 'facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookRequestException.php' );
+	require_once( 'facebook-php-sdk-v4-4.0-dev/src/Facebook/FacebookAuthorizationException.php' );
+	require_once( 'facebook-php-sdk-v4-4.0-dev/src/Facebook/GraphObject.php' );
 	
 	use Facebook\FacebookSession;
 	use Facebook\FacebookRedirectLoginHelper;
