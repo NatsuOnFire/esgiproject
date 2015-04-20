dc22a2ed8dc821ae7a3b7fbc58f822159d4d351d
Florian <laforest.florian@gmail.com>
2015-04-20 09:57:07 +0200

    like button

--- gridPhotos.php
+++ gridPhotos.php
@@ -22,7 +22,9 @@
         	<div class="thumbnail">
 				<img class="photo" src="<?php echo $image->images[0]->source; ?>"/>
 				<div class="caption">
-					<p><a href="#" class="btn btn-primary">Like</a></p>
+					<p>
+						<div class="fb-like" data-href="https://esgiproject2.herokuapp.com/like/<?php echo $image->id ?>" data-layout="button_count" data-action="like" data-show-faces="true" data-share="true"></div>
+					</p>
 				</div>
 			</div>
 		</div>
